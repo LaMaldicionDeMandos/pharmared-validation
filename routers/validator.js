ecc6eb38c6ac5afcd288480d8aae92ba053c5b01
Marcelo Pasut <pasutmarcelo@gmail.com>
2016-09-20 23:02:13 -0300

    Trabajo en validacion de farmacias

--- routers/validator.js
+++ routers/validator.js
@@ -2,9 +2,24 @@
  * Created by boot on 9/19/16.
  */
 var router = require('express').Router();
+var request = require('request');
+var PHARMACY_ACTIVITY_CODE = 477310;
+function validateCodes(codes, activities) {
+    return codes.every(code => activities.find(item => code == item));
+};
 var validatePharmacy = function(req, res) {
     var cuit = req.params.cuit;
-    return res.send({valid: true, name: 'pharmacy'});
+    request(config.afip_url + cuit, function(error, response, body) {
+        var result = JSON.parse(body);
+        if (error || !result.success) {
+            res.status(400).send(error);
+        } else {
+            console.log(body);
+            var isValid = result.data.actividades != null
+                && validateCodes([PHARMACY_ACTIVITY_CODE], result.data.actividades);
+            res.send({valid: isValid, name: result.data.nombre});
+        }
+    });
 };
 
 router.get('/pharmacy/:cuit', validatePharmacy);
