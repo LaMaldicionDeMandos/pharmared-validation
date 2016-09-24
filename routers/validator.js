9651c6886fe4d5dc94da7fec7644146eba3feb44
Marcelo Pasut <pasutmarcelo@gmail.com>
2016-09-23 22:35:29 -0300

    Valido que el estado de la clave es activo y que si es persona fisica no este fallecida

--- routers/validator.js
+++ routers/validator.js
@@ -4,12 +4,19 @@
 var router = require('express').Router();
 var request = require('request');
 var PHARMACY_ACTIVITY_CODE = 477310;
-//TODO Validar estadoClave == 'ACTIVO'
-//TODO Validar if tipoPersona == 'FISICA' => fechaFallecimiento == undefined
 //TODO Usar https://soa.afip.gob.ar/av/v1/vencimientos/<cuit> que data no este vacio
 function validateCodes(codes, activities) {
     return codes.every(code => activities.find(item => code == item));
 };
+
+function validateState(item) {
+    return item.data.estadoClave == 'ACTIVO';
+};
+
+function validateisDead(item) {
+    return item.data.tipoPersona != 'FISICA' || !item.data.fechaFallecimiento;
+};
+
 var validatePharmacy = function(req, res) {
     var cuit = req.params.cuit;
     request(config.afip_url + cuit, function(error, response, body) {
@@ -18,8 +25,9 @@ var validatePharmacy = function(req, res) {
             res.status(400).send(error);
         } else {
             console.log(body);
-            var isValid = result.data.actividades != null
-                && validateCodes([PHARMACY_ACTIVITY_CODE], result.data.actividades);
+            var isValid = result.data.actividades != null &&
+                validateCodes([PHARMACY_ACTIVITY_CODE], result.data.actividades) &&
+                validateState(result) && validateisDead(result);
             res.send({valid: isValid, name: result.data.nombre});
         }
     });
